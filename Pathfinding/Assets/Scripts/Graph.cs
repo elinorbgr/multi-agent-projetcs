ef9eaa62ecaa09191e02ca84cd5d68c4824ed7c5
Victor Berger <victor.berger@m4x.org>
2015-01-26 10:39:33 +0100

    Correct Graph class.

--- Pathfinding/Assets/Scripts/Graph.cs
+++ Pathfinding/Assets/Scripts/Graph.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Graph {
 
 	public class Node {
-        public Vector3 pos;
-        public List<Node> neighbors;
+		public Vector3 pos;
+		public List<Node> neighbors;
 
-        public Node(Vector3 pos) {
-            this.pos = pos;
-            this.neighbors = new List();
-        }
+		public Node(Vector3 pos) {
+			this.pos = pos;
+			this.neighbors = new List<Node>();
+		}
 
-        public connect(Node other) {
-            this.neighbors.add(other);
-            other.neighbors.add(this);
-        }
-    }
+		public void connect(Node other) {
+			this.neighbors.Add(other);
+			other.neighbors.Add(this);
+		}
+	}
 
 }
