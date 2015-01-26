b55f5fb8b33185e9024b9ae5803f62f07d9c6782
Victor Berger <victor.berger@m4x.org>
2015-01-26 08:48:34 +0100

    Graph representation.

---
+++ Pathfinding/Assets/Scripts/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public class Graph {
+
+	public class Node {
+        public Vector3 pos;
+        public List<Node> neighbors;
+
+        public Node(Vector3 pos) {
+            this.pos = pos;
+            this.neighbors = new List();
+        }
+
+        public connect(Node other) {
+            this.neighbors.add(other);
+            other.neighbors.add(this);
+        }
+    }
+
+}
