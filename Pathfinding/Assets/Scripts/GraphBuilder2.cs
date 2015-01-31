27a1e2680bbef020f7691366440975f16378ed3e
MaximWolpher <maximwolpher@gmail.com>
2015-01-31 22:10:57 +0100

    GraphBuilders
    GraphBuilder2 is for kinematic and dynamic.

---
+++ Pathfinding/Assets/Scripts/GraphBuilder2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphBuilderKinematic : MonoBehaviour {
+
+	private Graph g;
+
+	// Use this for initialization
+	void Start () {
+		g = new Graph();
+
+		// create a grid of nodes
+		for(int i = -4; i <= 4; i += 2){
+			for(int j = -4; j <= 4; j += 2){
+				g.nodes.Add(new Graph.Node(new Vector3(i, 0.5F,j)));
+			}
+
+		}
+		// connect the nodes that can see each other
+		foreach(Graph.Node n in g.nodes){
+			foreach(Graph.Node m in g.nodes){
+				Vector3 direction = m.pos - n.pos;
+				float distance = direction.magnitude;
+				if(distance!=0 && !Physics.Raycast(n.pos, direction, distance+0.25f)){ 
+					// The 0.25 constant is to give some space next to the wall												
+					n.connect(m);
+				}
+
+			}
+			
+		}
+
+		g.CreateDisplayers();
+
+	}
+	public Graph getGraph() {
+		return this.g;
+	}
+
+}
---
+++ Pathfinding/Assets/Scripts/GraphCreatorDiscrete.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphCreatorDiscrete: MonoBehaviour {
+	
+	private Graph g;
+	
+	// Use this for initialization
+	void Start () {
+		g = new Graph();
+		
+		for(int i = -4; i <= 4; i += 2){
+			for(int j = -4; j <= 4; j += 2){
+				g.nodes.Add(new Graph.Node(new Vector3(i, 0.5F,j)));
+			}
+			
+		}
+		foreach(Graph.Node n in g.nodes){
+			foreach(Graph.Node m in g.nodes){
+				Vector3 direction = m.pos - n.pos;
+				float distance = direction.magnitude;
+				if(distance == 2 && !Physics.Raycast(n.pos, direction, distance+0.25f)){
+					//the "distance == 2" tells the nodes to connect only their adjacent nodes
+					n.connect(m);
+				}
+				
+			}
+			
+		}
+		
+		g.CreateDisplayers();
+		
+	}
+	public Graph getGraph() {
+		return this.g;
+	}
+	
+}
